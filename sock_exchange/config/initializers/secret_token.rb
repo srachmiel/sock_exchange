afd775057f8bb5c09f50fc39867ee67d878152b4
Sarah Rachmiel <srachmiel2014@kellogg.northwestern.edu>
2013-10-21 18:30:28 -0500

    Homework 4: Part III

---
+++ sock_exchange/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SockExchange::Application.config.secret_key_base = '377a43ae582aa16b0ea8ae51e9a3602ee69f8cfd546376f9cd37844361545c4a8470a975f57d8c39134168750f387930154d745ced281f536d0c2a0a084b89cf'
---
+++ sock_exchange/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SockExchange::Application.config.session_store :cookie_store, key: '_sock_exchange_session'
